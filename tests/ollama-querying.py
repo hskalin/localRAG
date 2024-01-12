0d4c393773e2b1da8a44bda42a6c0803dd0e59cc
hskalin <nilaksh00072@gmail.com>
2024-01-12 18:21:27 +0530

    added context

--- tests/ollama-querying.py
+++ tests/ollama-querying.py
@@ -7,31 +7,48 @@ parser = argparse.ArgumentParser()
 
 # Adding optional argument
 parser.add_argument("-m", "--model", help="llm model", type=str, default="tinyllama")
-parser.add_argument("-p", "--prompt", help="model prompt", type=str, required=True)
+parser.add_argument("-p", "--prompt", help="model prompt", type=str, required=False)
 
 # Read arguments from command line
 args = parser.parse_args()
 
-url = "http://localhost:11434/api/generate"
+SERVER_URL = "http://localhost:11434"
 
-# Define the payload (data to be sent with the request)
-payload = {
-    "model": args.model,
-    "prompt": args.prompt
-}
+def generate_response(prompt, model='tinyllama', context=[]):
+    API_END = "/api/generate"
 
-# Send the POST request with streaming
-with requests.post(url, json=payload, stream=True) as response:
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Iterate through the streaming responses
-        for line in response.iter_lines():
-            if line:
-                # Parse the JSON response
-                data = json.loads(line.decode("utf-8"))
-                
-                # Print without buffering
-                print(data["response"], end="", flush=True)
-        print("")
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+    payload = {
+        "model": model,
+        "prompt": prompt,
+        "context": context
+    }
+
+    url = SERVER_URL + API_END
+    next_context = []
+
+    # Send the POST request with streaming
+    with requests.post(url, json=payload, stream=True) as response:
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Iterate through the streaming responses
+            for line in response.iter_lines():
+                if line:
+                    # Parse the JSON response
+                    data = json.loads(line.decode("utf-8"))
+                    
+                    # Print without buffering
+                    print(data["response"], end="", flush=True)
+                    if data["done"]: 
+                        next_context = data["context"]
+
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+    return next_context
+ 
+n = 4
+cont = []
+while(n):
+    promp = input("\n>>> ")
+    cont = generate_response(promp, args.model, cont)
+    n -= 1
